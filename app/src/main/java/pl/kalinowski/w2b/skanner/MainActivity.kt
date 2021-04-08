d92b4bbdb0b70c836f0dfe15b942d86dcc0a6aac
Wojciech Kalinowski <wojciechkalinowskipoczta@gmail.com>
2021-04-08 15:54:57 +0200

    adding opencv

--- app/src/main/java/pl/kalinowski/w2b/skanner/MainActivity.kt
+++ app/src/main/java/pl/kalinowski/w2b/skanner/MainActivity.kt
@@ -3,9 +3,14 @@ package pl.kalinowski.w2b.skanner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
